0e1a614acc4fe7dea751eb95fb8e5158cf0cf78b
mustafadennis <96393574+mustafadennis@users.noreply.github.com>
2023-06-20 19:09:23 +0300

    Initial commit node from scratch

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-// POST -- send data to server
-// GET - get data from server
-// PUT - update data on server
-// DELETE - delete data on server
-// PATCH - update data on server (partial update)
-
-import express from "express";
-const app = express();
-
-app.use(express.json());
+import http from "http";
+import url from "url";
 
 const users = [
   { id: 1, name: "Zymante" },
@@ -16,39 +8,42 @@ const users = [
   { id: 4, name: "Kristina" },
   { id: 5, name: "Vaida" },
 ];
-
 const port = 5000;
-app.get("/", (request, response) => {
-  response.send("Response from server (MAIN ROUTE)");
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+
+  if (req.method === "GET") {
+    if (parsedUrl.pathname === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Response from server (MAIN ROUTE)");
+    }
+
+    if (parsedUrl.pathname === "/users") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(users));
+    }
+  }
+
+  if (req.method === "POST") {
+    if (parsedUrl.pathname === "/create-user") {
+      let requestFromUser = "";
+
+      req.on("data", (chunk) => {
+        requestFromUser += chunk;
+      });
+
+      req.on("end", () => {
+        const newUser = JSON.parse(requestFromUser);
+        users.push(newUser);
+      });
+
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(users));
+    }
+  }
 });
 
-app.get("/users", (request, response) => {
-  response.json(users);
-});
-
-app.post("/create-user", (request, response) => {
-  const requestFromUser = request.body;
-  users.push(requestFromUser);
-  response.status(201).json(users);
-});
-
-app.put("/update-user/:userId", (request, response) => {
-  const requestFromUser = request.body;
-
-  const userId = Number(request.params.userId);
-
-  const updatedUsers = users.filter((user) => user.id !== userId);
-  updatedUsers.push(requestFromUser);
-  response.status(200).json(updatedUsers);
-});
-
-app.delete("/delete-user/:userId", (request, response) => {
-  const userId = Number(request.params.userId);
-
-  const updatedUsers = users.filter((user) => user.id !== userId);
-  response.status(200).json(updatedUsers);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} `);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
